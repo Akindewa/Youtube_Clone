fc3dc3413702a0bd628ecc53d12d2f1c10b0a459
Akindewa <raodhotakindewa@gmail.com>
2024-10-12 10:30:30 +0100

    addded-new-features

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,10 @@
+import React from "react";
 import { Stack } from '@mui/material'
 
 import { categories } from '../utils/constants';
 
 
-const Sidebar = ({selectedCategory, setSelectedCategory}) =>  (
-
+const Categories = ({selectedCategory, setSelectedCategory}) => (
     <Stack
       direction='row'
       sx={{
@@ -13,25 +13,21 @@ const Sidebar = ({selectedCategory, setSelectedCategory}) =>  (
         flexDirection: { md: 'column'},
       }}    
     >
-
     {categories.map((category) => (
         <button
           className='category-btn'
           onClick={() => setSelectedCategory(category.name)}
           style={{
             background: category.name === selectedCategory && '#FC1503',
-            color: 'white'
+            color: 'white',
           }}
-          key= {category.name}
-        
+          key= {category.name}       
         >
             <span style={{color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px'}}>{category.icon}</span>
             <span style={{ opacity: category.name === selectedCategory ? '1' : '0.8' }}>{category.name}</span>
         </button>
     ) )}
-
     </Stack>
-  )
+);
 
-
-export default Sidebar
+export default Categories;
